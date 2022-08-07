0056ea4bf8e5e0fd6c563f1e879c019f6f071c94
Harry Chappell <harry@hcdigital.co.uk>
2022-08-07 12:32:18 +0100

    Test one Advanced section (20 mins)

--- page-bing-bang.php
+++ page-bing-bang.php
@@ -21,6 +21,9 @@
 
 				<main id="main" class="" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
 
+					<p>Initial test was just a simple ifelse statement that took 10 minutes. The more advanced version is below and took about a further 20 minutes. If you edit this page you can input attributes into the ACF fields, and the following list of numbers will change accordingly.</p>
+					<br>
+					
 					<?php 
 
 						// For loop to count to 100
@@ -29,17 +32,40 @@
 						// }
 
 						// Same thing but as an elseif functino to check for mutiples of 3 and 5.
+						// for ($i = 1; $i <= 100; $i++) {
+						// 	if ($i % 3 == 0) {
+						// 		echo "Bing";
+						// 	} elseif ($i % 5 == 0) {
+						// 		echo "Bang";
+						// 	} else {
+						// 		echo $i;
+						// 	}
+							
+						// 	echo "<br>";
+						// }
+
+						// More advanced version with variables	
+						$firstNo = get_field('first_no');
+						$firstText = get_field('first_text');
+						$secondNo = get_field('second_no');
+						$secondText = get_field('second_text');
+						$min = get_field('min');
+						$max = get_field('max');
+						
+						echo '<div class="test-result-cntr">';
 						for ($i = 1; $i <= 100; $i++) {
-							if ($i % 3 == 0) {
-								echo "Bing";
-							} elseif ($i % 5 == 0) {
-								echo "Bang";
+							echo "<p>";
+							if ($i % $firstNo == 0 & $i >= $min & $i <= $max) {
+								echo $firstText;
+							} elseif ($i % $secondNo == 0 & $i >= $min & $i <= $max) {
+								echo $secondText;
 							} else {
 								echo $i;
 							}
 							
-							echo "<br>";
+							echo "</p>";
 						}
+						echo '</div>';
 					?>
 
 				</main>
