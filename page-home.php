7e33d6a4ae4ad5319be08360b7f08537f0441a6a
Harry Chappell <harry@hcdigital.co.uk>
2022-08-07 22:02:33 +0100

    Adding ACF fields to build the page

--- page-home.php
+++ page-home.php
@@ -19,11 +19,77 @@
 
 		<div id="inner-content" class="wrap">
 
-				<main id="main" class="" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
+			<main id="main" class="" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
 
-					
+				<section class="hero" style="background-image: url(<?php the_field('hero_image'); ?>);">
+					<h2><?php the_field('hero_title'); ?></h2>
+					<h3><?php the_field('hero_subtitle'); ?></h3>
+				</section>
 
-				</main>
+				<section class="section-2 
+				text-colour-<?php the_field('section_2_text_colour'); ?> 
+				text-align-<?php the_field('section_2_text_align'); ?>"
+				<?php if (get_field('section_2_background') == 'image') { 
+					echo 'style="background-image: url(' . get_field('section_2_background_image') . ');"';
+					} elseif (get_field('section_2_background') == 'colour') {
+					echo 'style="background-color: ' . get_field('section_2_background_colour') . ';"';
+					}; ?>
+				>
+
+					<h2><?php the_field('section_2_title'); ?></h2>
+					<?php the_field('section_2_body'); ?>
+
+				</section>
+				</section>
+
+				<section class="section-3 
+				text-colour-<?php the_field('section_3_text_colour'); ?> 
+				text-align-<?php the_field('section_3_text_align'); ?>"
+				<?php if (get_field('section_3_background') == 'image') { 
+					echo 'style="background-image: url(' . get_field('section_3_background_image') . ');"';
+					} elseif (get_field('section_3_background') == 'colour') {
+					echo 'style="background-color: ' . get_field('section_3_background_colour') . ';"';
+					}; ?>
+				>
+
+					<h2><?php the_field('section_3_title'); ?></h2>
+					<?php the_field('section_3_body'); ?>
+
+				</section>
+				</section>
+
+				<section class="section-4 
+				text-colour-<?php the_field('section_4_text_colour'); ?> 
+				text-align-<?php the_field('section_4_text_align'); ?>"
+				<?php if (get_field('section_4_background') == 'image') { 
+					echo 'style="background-image: url(' . get_field('section_4_background_image') . ');"';
+					} elseif (get_field('section_4_background') == 'colour') {
+					echo 'style="background-color: ' . get_field('section_4_background_colour') . ';"';
+					}; ?>
+				>
+
+					<h2><?php the_field('section_4_title'); ?></h2>
+					<?php the_field('section_4_body'); ?>
+
+				</section>
+				</section>
+
+				<section class="section-5 
+				text-colour-<?php the_field('section_5_text_colour'); ?> 
+				text-align-<?php the_field('section_5_text_align'); ?>"
+				<?php if (get_field('section_5_background') == 'image') { 
+					echo 'style="background-image: url(' . get_field('section_5_background_image') . ');"';
+					} elseif (get_field('section_5_background') == 'colour') {
+					echo 'style="background-color: ' . get_field('section_5_background_colour') . ';"';
+					}; ?>
+				>
+
+					<h2><?php the_field('section_5_title'); ?></h2>
+					<?php the_field('section_5_body'); ?>
+
+				</section>
+
+			</main>
 
 		</div>
 
